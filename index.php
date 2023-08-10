d958973b354999fe90fdb0c5297ade8c41aeef8d
Michal Tobor <michal.tobor1@gmail.comgit config -- global user.name MTWF1lsgit statusgit commitgit config --global user.email michal.tobor1@gmail.com>
2023-08-10 09:28:23 +0200

    nowy commit

---
+++ index.php
@@ -0,0 +1,85 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <title>Rozpoczęcie pracy</title>
+        <link rel="stylesheet" href="styl.css">
+</head>
+<body>
+    <h1>Rozpoczęcie pracy</h1>
+
+    <?php
+    session_start();
+
+    // Dane bazy danych
+    $host = "localhost"; 
+    $username = "root";  
+    $password = "";  
+    $dbname = "zgloszenie"; 
+
+    // Tworzenie połączenia z bazą danych
+    $conn = new mysqli($host, $username, $password, $dbname);
+
+    // Sprawdzenie czy udało się połączyć z bazą danych
+    if ($conn->connect_error) {
+        die("Błąd połączenia z bazą danych: " . $conn->connect_error);
+    }
+
+    // Sprawdzenie czy użytkownik jest zalogowany
+    if (!isset($_SESSION['rcp'])) {
+        header("Location: logowanie.php"); // Przekierowanie do strony logowania
+        exit();
+    }
+
+    // Obsługa formularza rozpoczęcia pracy
+    if ($_SERVER["REQUEST_METHOD"] == "POST") {
+        $rcp = $_SESSION['rcp'];
+        $numer = $_POST['numer'];
+
+        // Sprawdzenie, czy numer jest poprawny (1, 2, 3, 4)
+        if ($numer >= 1 && $numer <= 4) {
+            // Tutaj można dodać kod zapisujący zgłoszenie rozpoczęcia pracy do bazy danych
+            // Przykładowe zapytanie SQL:
+            $sql = "INSERT INTO zgloszenia (date, rcp, numer) VALUES ('$rcp', '$numer', '$date')";
+            if ($conn->query($sql) === TRUE) {
+                echo "Rozpoczęto pracę: Numer $numer" "Data i godzina rozpoczęcia: $date";
+            } else {
+                echo "Błąd: " . $sql . "<br>" . $conn->error;
+            }
+        } else {
+            echo "Niepoprawny numer.";
+        }
+    }
+
+    // Pobierz dane pracownika z bazy
+
+    $rcp = $_SESSION['rcp'];
+    $sql = "SELECT * FROM pracownicy WHERE RCP = '$rcp'";
+    $result = $conn->query($sql);
+    $row = $result->fetch_assoc();
+    
+    //data zgłoszenia
+    
+
+    // Wyświetl formularz
+    echo "<div class='container'>";
+    echo "<p>Witaj, " . $row['imie'] . " " . $row['nazwisko'] . "!</p>";
+    echo "Rozpocząłeś pracę" ;
+    echo "<form method='post'>";
+    echo "<label for='numer'>Wybierz numer:</label>";
+    echo "<select id='numer' name='numer'>";
+    echo "<option value='1'>1</option>";
+    echo "<option value='2'>2</option>";
+    echo "<option value='3'>3</option>";
+    echo "<option value='4'>4</option>";
+    echo "</select>";
+    echo "<br>";
+    echo "<input type='submit' value='Rozpocznij'>";
+    echo "</form>";
+    echo "</div>";
+
+    // Zamknięcie połączenia z bazą danych
+    $conn->close();
+    ?>
+
+</body>
+</html>
---
+++ logowanie.php
@@ -0,0 +1,56 @@
+<?php
+session_start();
+
+if ($_SERVER["REQUEST_METHOD"] == "POST") {
+    $rcp = $_POST["rcp"];
+    $pass = $_POST["pass"];
+
+    // Dane bazy danych (jak wcześniej)
+    $host = "localhost";
+    $username = "root";
+    $password = "";
+    $dbname = "zgloszenie";
+
+    $conn = new mysqli($host, $username, $password, $dbname);
+
+    if ($conn->connect_error) {
+        die("Błąd połączenia z bazą danych: " . $conn->connect_error);
+    }
+
+    // Sprawdzenie czy dane logowania są poprawne
+    $sql = "SELECT * FROM pracownicy WHERE RCP = '$rcp' AND pass = '$pass'";
+    $result = $conn->query($sql);
+
+    if ($result->num_rows == 1) {
+        $_SESSION['rcp'] = $rcp;
+        header("Location: index.php"); // Przekierowanie do strony rozpoczęcia pracy
+        exit();
+    } else {
+        echo "Błędne dane logowania.";
+    }
+
+    $conn->close();
+}
+?>
+
+<!DOCTYPE html>
+<html>
+
+<head>
+    <title>Logowanie</title>
+  <link rel="stylesheet" href="styl.css">
+</head>
+<body>
+    <h1>Logowanie</h1>
+
+    <form method="post">
+        <label for="rcp">RCP:</label>
+        <input type="text" id="rcp" name="rcp" required><br>
+
+        <label for="pass">Hasło:</label>
+        <input type="password" id="pass" name="pass" required><br>
+
+        <input type="submit" value="Zaloguj">
+    </form>
+</body>
+</html>
